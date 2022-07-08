13173c978dd2d9e51bbb3a25347f187a22e18947
Kamaraj Christopher <k.appadurai@spglobal.com>
2022-07-08 17:42:06 -0500

    Changes made during day 10

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,4 +1,5 @@
 # Calculator
+from art import logo
 # Add
 def add(n1, n2):
     return n1 + n2
@@ -19,19 +20,32 @@ def divide(n1, n2):
     return n1 / n2
 
 
-operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide,
-}
+def calculator():
+    print(logo)
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide,
+    }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
+    keep_going = True
+    num1 = float(input("What's the first number?: "))
+    while keep_going:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What's the next number?: "))
 
-func = operations[operation_symbol]
-answer = func(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        func = operations[operation_symbol]
+        answer = func(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        should_continue = input(f"If you want to continue with {answer}, type 'y' or type 'n' if you want to start a new calculation: ")
+        if should_continue == 'n':
+            keep_going = False
+            calculator()
+        else:
+            num1 = answer
+
+
+calculator()
